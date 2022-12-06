d5dd705aa8d17fae79447da4c68fcb40667fbb38
Gabriel Duta <47044489+GabrielDuta@users.noreply.github.com>
2022-12-06 13:35:21 +0100

    Added comments to day 5

--- 5_crates/5_crates.cpp
+++ 5_crates/5_crates.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <vector>
 #include <stack>
-#include <queue>
 using namespace std;
 
 vector<stack<char>> crates;
@@ -13,6 +12,7 @@ int main() {
   	int crates_number, moves;
 	int quantity = 0;
 	int start = 0, end = 0;
+	char tmp;
 
 	ifstream in("input.txt");
 	ofstream out("output.txt");
@@ -20,28 +20,33 @@ int main() {
 	in >> crates_number >> moves;
 	crates.resize(crates_number);
 
+	// Populate the stacks: in the file the format of a stack is: <quantity> <crates, char>
 	for (int i = 0; i < crates_number; i++){
 		in >> quantity;
 		for (int f = 0; f < quantity; f++) {
-			char tmp;
 			in >> tmp;
 			crates[i].push(tmp);
 		}
 	}
+
+	// Execute the moves; the file is clean of useless text
 	for(int i = 0; i < moves; i++) {
 		in >> quantity >> start >> end;
 		move(quantity, start - 1, end - 1);
 	}
 
+	// Print the crates at the top
 	for(int i = 0; i < crates_number; i++)
 		out << crates[i].top();
 
   return 0;
 }
 
+/* Solution 1: remove the stack */
 void move(int quantity, int start, int end) {
 	stack<char> s;
 
+	// Save the crates in the stack to move them without changing the order
 	for(int i = 0; i < quantity; i++) {
 		s.push(crates[start].top());
 		crates[start].pop();
